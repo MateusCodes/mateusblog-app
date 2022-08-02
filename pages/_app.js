03bbd3371737ccd533c18d34fc2fc226e0e42719
Mateus Sobreira <matheus.sobreira.27@gmail.com>
2022-08-01 22:55:00 -0300

    fixing some Head errors

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 import '../styles/globals.scss';
 import { Layout } from '../components';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    return (
+        <>
+            <Head>
+                <title>MateusBlog</title>
+                <meta
+                    name="description"
+                    content="MateusCodes® Blog usando NextJS com SSG"
+                />
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
 
 export default MyApp;

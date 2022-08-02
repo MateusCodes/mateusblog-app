c26ccf1ae49353e95b4a6a34e89e7aaf56c4edf5
Mateus Sobreira <matheus.sobreira.27@gmail.com>
2022-08-01 23:02:58 -0300

    fixing meta tag and html lang

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
                 <title>MateusBlog</title>
                 <meta
                     name="description"
-                    content="MateusCodes® Blog usando NextJS com SSG"
+                    content="MateusCodes® Blog usando NextJS com SSG. Aqui você encontra um layout de blog moderno jutamente com o poder de uma SPA com Static Site Generator"
                 />
             </Head>
             <Layout>
